6dd74e5d5577ce743471ca18782cc16edc11c7f8
SamuelArteaga <32024855+SamuelArteaga@users.noreply.github.com>
2019-02-16 22:35:00 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,67 +7,83 @@ using namespace std;
 
 int main()
 {
-	// gets user input 
-	string userInput;
-	string action, object; 
+	string userInput, action, object, concatenate;
 	cout << "login: ";
-	cin >> userInput; 
+	cin >> userInput;
 
-	vector<string> myVector;
 	multimap<string, string> userRoleMap;
-	multimap<string, string, string> permissionRoleMap;
+	multimap<string, string> permissionRoleMap;
+
 	fstream infile;
 	string info;
-
-	for (int i = 0; i < 2; i++) {
-		string filename; 
-		if (i == 0) {
-			filename = "URA.txt";
-		}
-		else if (i == 1) {
-			filename = "PRA.txt"; 
-		}
-		
-		infile.open(filename);
-		while (infile >> info) {
-			int nElem = 0;
-			auto it = myVector.insert(myVector.end(), info);
-			nElem++;
-		}
-		infile.close();
+	infile.open("URA.txt");
+	vector<string> myVector;
+	vector<string> vec; 
+	while (infile >> info) {
+		auto it = myVector.insert(myVector.end(), info);
 	}
+	infile.close();
 
 	for (int i = 0; i < myVector.size(); i += 2) {
-		string key = myVector.at(i);
-		string data = myVector.at(i + 1);
-		userRoleMap.insert(pair<string, string>(key, data));
+		string data1 = myVector.at(i);
+		string data2 = myVector.at(i + 1);
+		userRoleMap.insert(pair<string, string>(data1, data2));
 	}
+	myVector.clear();
 
-	for (int i = 0; i < myVector.size(); i += 2) {
-		string key = myVector.at(i);
-		string data1 = myVector.at(i + 1);
-		string data2 = myVector.at(i + 2); 
-		//string concat = data1 + " " + data2; 
-		permissionRoleMap.insert(pair<string, string>(key, concat));
-	}
-	
 	if (userRoleMap.count(userInput) > 0) {
-		cout << "Welcome " << userInput << "!" << endl; 
+		cout << "Welcome " << userInput << "!" << endl;
 		cout << "cmd>";
-		cin >> action >> object; 
-		
+		cin >> action >> object;
 	}
 	else {
-		cout << "ERROR: user " << userInput << " is not in the database!" << endl; 
+		cout << "ERROR: user " << userInput << " is not in the database!" << endl;
 	}
-	 
 
+	infile.open("PRA.txt");
+	while (infile >> info) {
+		auto it = myVector.insert(myVector.end(), info);
+	}
+	infile.close();
 
+	for (int i = 0; i < myVector.size(); i += 2) {
+		string data1 = myVector.at(i);
+		i = i + 1; 
+		string data2 = myVector.at(i) + " " + myVector.at(i + 1);
+		permissionRoleMap.insert(pair<string, string>(data1, data2));
+	}
+	myVector.clear(); 
+	concatenate = action + " " + object;
 
+	pair<multimap<string, string>::iterator, multimap<string, string>::iterator> iterPair = userRoleMap.equal_range(userInput);
 
+	for (multimap<string, string>::iterator listIt = iterPair.first;
+		listIt != iterPair.second; ++listIt){
+		myVector.insert(myVector.begin(),listIt->second);
+	}
+	
+	for (int i = 0; i < myVector.size(); i++) {
+		pair<multimap<string, string>::iterator, multimap<string, string>::iterator> iterPair = permissionRoleMap.equal_range(myVector.at(i));
+		for (multimap<string, string>::iterator listIt = iterPair.first;
+			listIt != iterPair.second; ++listIt) {
+			if (listIt->second == concatenate) {
+				vec.insert(vec.begin(), listIt->first);
+			}
+		}
+	}
 
+	if (!vec.empty()) {
 
-
+		cout << "Access granted by virtue of roles: ";
+		for (int i = 0; i < vec.size(); i++) {
+			cout << vec.at(i) << " "; 
+		}
+		cout << endl; 
+	}
+	else {
+		cout << "Access denied: you are not authorized to perform this action!" << endl;
+	}
+	
 
 	system("pause");
 	return 0;
